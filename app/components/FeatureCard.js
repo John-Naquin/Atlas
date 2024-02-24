e9360ab4c60f03590a6c9c02c0305be8d803c229
john <your-email@example.com>
2024-02-23 23:23:34 -0600

    Added About me descr and stuff to features

--- app/components/FeatureCard.js
+++ app/components/FeatureCard.js
@@ -7,32 +7,21 @@ const FeatureCard = ({ title, description }) => (
     </div>
 );
 
+
 const Features = () => {
     return (
-        <div className="relative text-center font-source-code-pro flex flex-col justify-center items-center h-screen p-10 text-black space-y-8">
+        <div className="flex flex-col justify-center items-center min-h-screen p-4 md:p-10 text-black space-y-4 md:space-y-8">
             <FeatureCard 
                 title="Personalized Interactions" 
-                description="Our AI chatbot adapts to your individual preferences and learning style, offering a uniquely personalized conversational experience." 
+                description="Our AI chatbot adapts to your individual preferences, offering a uniquely personalized experience." 
             />
             <FeatureCard 
                 title="24/7 Availability" 
-                description="Get assistance any time of the day! Our AI chatbot is available 24/7 to answer your queries, ensuring that help is always at your fingertips." 
-            />
-            <FeatureCard 
-                title="Multilingual Support" 
-                description="Communicate comfortably in your preferred language. Our AI chatbot supports multiple languages, making it accessible to a diverse range of users." 
-            />
-            <FeatureCard 
-                title="Instant Information Retrieval" 
-                description="Need information fast? Our chatbot swiftly navigates through vast data to provide you with accurate answers in seconds." 
+                description="Get assistance any time! Our AI chatbot is always available to answer your queries." 
             />
             <FeatureCard 
                 title="User-Friendly Interface" 
-                description="Enjoy an easy-to-use interface with intuitive navigation, making your interactions with our AI chatbot a breeze." 
-            />
-            <FeatureCard 
-                title="Secure and Private Conversations" 
-                description="Your privacy matters. Conversations with our AI chatbot are encrypted and secure, ensuring your data and personal information are always protected." 
+                description="Enjoy an intuitive and easy-to-use interface for seamless interactions." 
             />
         </div>
     );
