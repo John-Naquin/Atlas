5d292cc80d463354f4f1ed5ba35cac91b578f5af
john <your-email@example.com>
2024-02-23 23:27:50 -0600

    Fixed about card to say me and heading to say about

--- app/about/page.js
+++ app/about/page.js
@@ -5,13 +5,13 @@ function About() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <div className="text-center">
-                <h1 className="text-4xl font-bold mb-8">Me</h1>
+                <h1 className="text-4xl font-bold mb-8">About</h1>
                 <img 
                     src="/profileImages/profile.jpg" 
                     alt="About Us" 
                     className="w-40 h-40 mx-auto rounded-full shadow-lg mb-6 object-cover"
                 />
-                <DescriptionCard title="Our Mission">
+                <DescriptionCard title="Me">
                     <p>Im a Full Stack Developer and Software Developer with a bachelors in Computer Science, passionate about both tech and space. 🌎</p>
                 </DescriptionCard>
             </div>
