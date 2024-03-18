5d6ee0f87c97289e49100398181b60e02be2a3cf
Rookie1829 <johnnaquin14@gmail.com>
2024-03-18 16:21:53 -0500

    Fixing vercel error

--- app/about/page.js
+++ app/about/page.js
@@ -9,7 +9,7 @@ function About() {
                     <img src="/profileImages/profile.jpg" alt="Creator" className="w-full h-full object-cover rounded-lg" />
                 </div>
                 <div className="max-w-md md:ml-2">
-                    <p className="mb-6">Hello, I'm the creator behind the Atlas project. With years of experience in chatbot development and a passion for creating innovative solutions, I have designed Atlas to be a versatile platform that offers specialized tools for math, chatting, and writing. Stay tuned for future additions to the Atlas family.</p>
+                    <p className="mb-6">Hello, I am the creator behind the Atlas project. With years of experience in chatbot development and a passion for creating innovative solutions, I have designed Atlas to be a versatile platform that offers specialized tools for math, chatting, and writing. Stay tuned for future additions to the Atlas family.</p>
                     <div className="flex gap-4">
                     <a href="https://www.linkedin.com/in/john-naquin/" target="_blank" rel="noopener noreferrer"
                            className="text-customGray cursor-pointer bg-transparent border border-customGray py-2 px-8 rounded hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
