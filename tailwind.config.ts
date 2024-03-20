313a1f799634c40922142374ae60003486e61f8e
Rookie1829 <johnnaquin14@gmail.com>
2024-03-20 17:51:10 -0500

    Added gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,16 +11,18 @@ const config: Config = {
       customBlue: '#9978CF', 
       customDarkBlue: '#426B7F', 
       customGray: '#EEEEEE', 
-      customBlack: '#000000'
+      customBlack: '#000000',
     },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))', 
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))', 
+        'custom-linear-gradient': 'linear-gradient(to right, #9978CF, #426B7F)', 
+        'custom-radial-gradient': 'radial-gradient(circle, #9978CF, #426B7F)', 
       },
     },
   },
+  
   plugins: [],
 };
 
