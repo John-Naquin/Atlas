b7223ede63fbdd286b7b7c7338f906fffc2a90f2
Rookie1829 <johnnaquin14@gmail.com>
2024-03-20 18:41:27 -0500

    Blur here image and changed about page

--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About() {
     return (
-        <main className="bg-custom-linear-gradient text-customBlack min-h-screen flex flex-col items-center justify-center p-4 md:p-24">
+        <main className="bg-custom-linear-gradient text-customGray min-h-screen flex flex-col items-center justify-center p-4 md:p-24">
             <h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-8 text-center">Meet the Creator of Atlas</h1>
             <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl">
                 <div className="w-full md:w-60 h-60 rounded-lg shadow-lg mb-4 md:mb-0 md:mr-4">
@@ -12,15 +12,15 @@ function About() {
                     <p className="mb-4 md:mb-6">Hello, I am the creator behind the Atlas project. With years of experience in chatbot development and a passion for creating innovative solutions, I have designed Atlas to be a versatile platform that offers specialized tools for math, chatting, and writing. Stay tuned for future additions to the Atlas family.</p>
                     <div className="flex flex-col md:flex-row gap-2 md:gap-4">
                         <a href="https://www.linkedin.com/in/john-naquin/" target="_blank" rel="noopener noreferrer"
-                           className="text-customGray cursor-pointer bg-transparent border border-customGray py-2 px-4 md:px-8 rounded text-center hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
+                           className="text-customGray text-xl cursor-pointer bg-transparent border-2 border-customGray py-2 px-4 md:px-8 rounded text-center hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
                             LinkedIn
                         </a>
                         <a href="https://github.com/Rookie1829" target="_blank" rel="noopener noreferrer"
-                           className="text-customGray cursor-pointer bg-transparent border border-customGray py-2 px-4 md:px-8 rounded text-center hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
+                           className="text-customGray text-xl cursor-pointer bg-transparent border-2 border-customGray py-2 px-4 md:px-8 rounded text-center hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
                             GitHub
                         </a>
                         <a href="https://onedrive.live.com/?authkey=%21AHUoACmzIzQUuc0&cid=24BCD1F502D345BE&id=24BCD1F502D345BE%2110484&parId=24BCD1F502D345BE%2110393&o=OneUp" target="_blank" rel="noopener noreferrer"
-                           className="text-customGray cursor-pointer bg-transparent border border-customGray py-2 px-4 md:px-8 rounded text-center hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
+                           className="text-customGray text-xl cursor-pointer bg-transparent border-2 border-customGray py-2 px-4 md:px-8 rounded text-center hover:bg-customBlack hover:text-customGray hover:border-customBlack transition duration-300 ease-in-out">
                             Resume
                         </a>
                     </div>
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -6,15 +6,15 @@ const Hero = () => {
 
   return (
     <div className="relative h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url(${bgImageUrl})` }}>
-      <div className="flex flex-col justify-center items-center h-full bg-opacity-50 bg-black px-4">
-        <h1 className="text-3xl md:text-5xl font-bold mb-4 md:mb-6 text-center text-customGray">Welcome to Atlas: Your Ultimate Chatbot Solution</h1>
-        <p className="text-lg md:text-xl mb-6 md:mb-8 max-w-md md:max-w-lg text-center text-customGray">Experience the power of AI-driven chatbots for math, chatting, and writing. Explore the future of conversational technology.</p>
+      <div className="flex flex-col justify-center items-center h-full bg-opacity-60 bg-black backdrop-blur-sm px-4">
+        <h1 className="text-3xl md:text-5xl font-bold mb-4 md:mb-6 text-center text-white">Welcome to Atlas: Your Ultimate Chatbot Solution</h1>
+        <p className="text-lg md:text-xl mb-6 md:mb-8 max-w-md md:max-w-lg text-center text-white">Experience the power of AI-driven chatbots for math, chatting, and writing. Explore the future of conversational technology.</p>
         <div className="flex flex-col md:flex-row gap-4 items-center">
           <Link href="/about">
-            <span className="inline-block cursor-pointer bg-transparent border-2 border-white text-customGray py-2 px-6 md:px-8 rounded transition duration-300 ease-in-out shadow-lg hover:bg-customBlack hover:text-white hover:border-customBlack text-center w-48">Learn More</span>
+            <span className="inline-block cursor-pointer bg-transparent border-2 border-white text-white py-2 px-6 md:px-8 rounded transition duration-300 ease-in-out shadow-lg hover:bg-customBlack hover:text-white hover:border-customBlack text-center w-48">Learn More</span>
           </Link>
           <Link href="/chat">
-            <span className="inline-block cursor-pointer bg-transparent border-2 border-white text-customGray py-2 px-6 md:px-8 rounded transition duration-300 ease-in-out shadow-lg hover:bg-customBlack hover:text-white hover:border-customBlack text-center w-48">Chat Now</span>
+            <span className="inline-block cursor-pointer bg-transparent border-2 border-white text-white py-2 px-6 md:px-8 rounded transition duration-300 ease-in-out shadow-lg hover:bg-customBlack hover:text-white hover:border-customBlack text-center w-48">Chat Now</span>
           </Link>
         </div>
       </div>
